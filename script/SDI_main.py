c307d79d1b6e213a37f6ec48997f161237f27a96
john <c.h.johnkim@gmail.com>
2019-09-11 12:42:39 +0800

    object visual during regrasp

--- script/SDI_main.py
+++ script/SDI_main.py
@@ -41,30 +41,56 @@ if __name__ == '__main__':
         delta_0 = config['delta_0']
         psi_regrasp = config['psi_regrasp']
         theta_tilt = config['theta_tilt']
+        tuck_angle = config['tuck']
         axis =  config['axis']
         object_thickness = config['object_thickness']
         object_length = config['object_length']
         tcp2fingertip = config['tcp2fingertip']
+        sim = config['sim']
         table_height_wrt_world = -0.02
 
-        p = group.get_current_pose().pose
-        trans_tool0 = [p.position.x, p.position.y, p.position.z]
-        rot_tool0 = [p.orientation.x, p.orientation.y, p.orientation.z, p.orientation.w] 
-        T_wg = tf.TransformerROS().fromTranslationRotation(trans_tool0, rot_tool0)
-        P_gg_target = [0, 0, tcp2fingertip+object_length-delta_0, 1]
-        P_wg_target = np.matmul(T_wg, P_gg_target)
-        #target_pose = np.concatenate((P_wg_target[0:3], rot_tool0), axis=None)
-
+        # If simulation, set robot initial position;
+        # Else, read position from real robot. 
+        if sim == 1:
+            pose = [-0.21, 0.830, 0.01+tcp2fingertip+object_length-delta_0, 0.7071, 0, -0.7071, 0]
+            motion_primitives.set_pose(pose)
+        else:
+            p = group.get_current_pose().pose
+            trans_tool0 = [p.position.x, p.position.y, p.position.z]
+            rot_tool0 = [p.orientation.x, p.orientation.y, p.orientation.z, p.orientation.w] 
+            T_wg = tf.TransformerROS().fromTranslationRotation(trans_tool0, rot_tool0)
+            P_gg_target = [0, 0, tcp2fingertip+object_length-delta_0, 1]
+            P_wg_target = np.matmul(T_wg, P_gg_target)
+        
+        # Set TCP speed     
         group.set_max_velocity_scaling_factor(tcp_speed)
-        #pose = [-0.21, 0.830, 0.01+tcp2fingertip+object_length-delta_0, 0.7071, 0, -0.7071, 0]
-        #motion_primitives.set_pose(pose)
-        Robotiq.goto(robotiq_client, pos=object_thickness+0.004, speed=config['gripper_speed'], force=config['gripper_force'], block=False) 
-        rospy.sleep(5)
+        
+        # Set gripper position
         Robotiq.goto(robotiq_client, pos=object_thickness, speed=config['gripper_speed'], force=config['gripper_force'], block=False)   
+        
         center = [pose[0], pose[1], pose[2] - tcp2fingertip - object_length + delta_0]
-        tilt.tilt(center, axis, int(90-theta_0), tcp_speed+0.02)
+        visualization.visualizer(center, 1, 0.01, 2)
+        
+        # Tilt
+        tilt.tilt(center, axis, int(90-theta_0), tcp_speed)
+        
+        # Visualize object during regrasp
+        p = group.get_current_pose().pose
+        object_v = [center[0]-p.position.x, center[1]-p.position.y, center[2]-p.position.z]
+        object_uv = object_v / np.sum(np.power(object_v,2))**0.5
+        object_edge = np.multiply(object_uv, object_length)
+        visualization.thin_object(center, np.subtract(center, object_edge), object_thickness, 3)
+        visualization.visualizer(np.subtract(center, object_edge), 1, 0.01, 4)
+        
+        # Regrasp
         regrasp.regrasp(np.multiply(axis, -1), int(psi_regrasp), tcp_speed)
-        tilt.tilt(center, axis, int(theta_tilt), tcp_speed+0.02)
-        tuck.rotate_tuck(np.multiply(axis, -1), int(theta_0-theta_tilt+10), 0.03, tcp_speed+0.02)
+
+        # Tilt
+        tilt.tilt(center, axis, int(theta_tilt), tcp_speed)
+        
+        # Tuck
+        tuck.rotate_tuck(np.multiply(axis, -1), int(tuck_angle), 0.03, tcp_speed)
+
+        rospy.spin()
         
     except rospy.ROSInterruptException: pass
